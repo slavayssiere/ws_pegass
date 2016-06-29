199b40bf49737ad6e3fd15ccfa8aa2c4e800e03a
Sébastien Lavayssiere <slavayssiere@fastbooking.net>
2016-06-29 16:16:40 +0200

    ca marche :)

--- statformateur.rb
+++ statformateur.rb
@@ -11,23 +11,51 @@ class StatsFormateur
     end
     
     def listthisyear(ul)
+        ret = {}
         beginOfYear = Date.parse("#{Date.today.year}-01-01")
         endOfYear = Date.parse("#{Date.today.year}-01-31")
         listsession = @pegass.callUrl("/crf/rest/seance?debut=#{Date.today.year}-01-01&fin=#{Date.today.year}-12-31&libelleLike=PSC1&page=0&pageInfo=true&perPage=1000&structure=#{ul}&typeActivite=-2")
-        
-        sessions = {}
+        compteur = {}
         listsession['list'].each do |session|
-            puts session
-            inscription_session = @pegass.callUrl("/crf/rest/seance/#{session['id']}/inscription")
-            puts inscription_session
-            if(inscription_session['role'].eql? "FORMATEUR")                
-                user = @pegass.callUrl("/crf/rest/utilisateur/#{inscription['utilisateur']['id']}")
-                puts "Session: #{user['prenom']} #{user['nom']}"
+            begin           
+                inscription_activite = @pegass.callUrl("/crf/rest/activite/#{session['activite']['id']}")                
+                if(inscription_activite['statut'].eql? 'Clos')
+                    begin
+                        inscription_sessions = @pegass.callUrl("/crf/rest/seance/#{session['id']}/inscription")
+                        if(!inscription_sessions.nil?)
+                            inscription_sessions.each do |inscription_session|
+                                if(inscription_session['role'].eql? "FORMATEUR")
+                                    user = @pegass.callUrl("/crf/rest/utilisateur/#{inscription_session['utilisateur']['id']}")
+                                    puts "Session: #{user['prenom']} #{user['nom']}"
+                                    if(!compteur[inscription_session['utilisateur']['id']].nil?)
+                                        compteur[inscription_session['utilisateur']['id']]=compteur[inscription_session['utilisateur']['id']]+1
+                                        ret[inscription_session['utilisateur']['id']] = {
+                                            :nombre => compteur[inscription_session['utilisateur']['id']],
+                                            :prenom => user['prenom'],
+                                            :nom => user['nom']
+                                        }
+                                    else
+                                        compteur[inscription_session['utilisateur']['id']]=1 
+                                        ret[inscription_session['utilisateur']['id']] = {
+                                            :nombre => compteur[inscription_session['utilisateur']['id']],
+                                            :prenom => user['prenom'],
+                                            :nom => user['nom']
+                                        }   
+                                    end                                    
+                                end
+                            end
+                        end
+                    rescue => detail
+                        puts detail
+                    end
+                end
+            rescue => detail
+                puts detail
             end
         end
         
-        
-        return sessions
+        puts ret
+        return ret
     end
     
     def getEmailList(list_nivol)        
