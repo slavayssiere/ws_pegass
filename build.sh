a2d119b97b5f65dca0dc2a713f6b12f15760ce6d
Sebastien Lavayssiere <sebastien.lavayssiere@wescale.fr>
2017-08-11 10:14:20 +0200

    add no cache in build

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ cp ./version_tpl.json ./version.json
 
 sed -i.bak s/VERSION/$version/g ./version.json
 
-docker build -t slavayssiere/ws_pegass:$version .
+docker build --no-cache -t slavayssiere/ws_pegass:$version .
 
 
